aa152bf910083f12cc26c7dcbb9c5c26e330b8a9
Kadeem Lewis <71411720+kadeem-lewis@users.noreply.github.com>
2024-11-12 20:44:56 -0500

    1 upgrade frontend to vite (#18)
    * installed packages needed for vite react typescript swc plugin
    
    * removed react scripts and updated npm scripts to use vite
    
    * created vite config
    
    * updated index.js to use new way of rendering react app and enabled strict mode
    
    * renamed index.js to main.tsx
    
    * updated react and react-dom to latest versions and specified type module in package.json
    
    * changed all react components fr4om using js extensions to tsx extensions
    
    * added an index.html file to serve as entry point for app
    
    * updated env usage in component to match the way vite handles envs
    
    * updated imports to components with extension changes
    
    * copied vite react typescript setup from vite example project
    
    * updated gitignore
    
    * updated changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+## 11-12-2024
+
+### Added
+
+- typescript support to react frontend
+
+### Changed
+
+- react packages to latest version
+- react component extensions from .js to .tsx
+- react frontend bundler from create-react-app to vite
+
+### Other
+
+- Enabled Strict Mode for React
--- frontend/src/services/moviesDataService.js
+++ frontend/src/services/moviesDataService.js
@@ -3,44 +3,46 @@ import axios from "axios";
 class MovieDataService {
   getAll(page = 0) {
     return axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies?page=${page}`
+      `${import.meta.env.VITE_BACKEND_URL}/api/v1/movies?page=${page}`
     );
   }
 
   get(id) {
     return axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies/id/${id}`
+      `${import.meta.env.VITE_BACKEND_URL}/api/v1/movies/id/${id}`
     );
   }
   find(query, by = "title", page = 0) {
     return axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies?${by}=${query}&page=${page}`
+      `${
+        import.meta.env.VITE_BACKEND_URL
+      }/api/v1/movies?${by}=${query}&page=${page}`
     );
   }
 
   createReview(data) {
     return axios.post(
-      `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies/review`,
+      `${import.meta.env.VITE_BACKEND_URL}/api/v1/movies/review`,
       data
     );
   }
 
   updateReview(data) {
     return axios.put(
-      `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies/review`,
+      `${import.meta.env.VITE_BACKEND_URL}/api/v1/movies/review`,
       data
     );
   }
   deleteReview(id, userId) {
     return axios.delete(
-      `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies/review`,
+      `${import.meta.env.VITE_BACKEND_URL}/api/v1/movies/review`,
       { data: { review_id: id, user_id: userId } }
     );
   }
 
   getRatings() {
     return axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies/ratings`
+      `${import.meta.env.VITE_BACKEND_URL}/api/v1/movies/ratings`
     );
   }
 }
